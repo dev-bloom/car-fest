182bbee369c6cf05905fa281add660276bd8fdcf
Alejandro Chaves <github@alejandrochaves.dev>
2023-06-18 18:26:58 -0500

    Updates

--- assets/ios.transition-legacy-b9e87851.js
+++ assets/ios.transition-legacy-b9e87851.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-52496634.js"],(function(e,t){"use strict";var o,n;return{setters:[e=>{o=e.l,n=e.m}],execute:function(){
+System.register(["./index-legacy-5e7aa363.js"],(function(e,t){"use strict";var o,n;return{setters:[e=>{o=e.l,n=e.m}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
